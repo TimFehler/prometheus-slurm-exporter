7b061fb197dc7287475658d5fff95eb15085ef0d
Tim Fehler <126073245+TimFehler@users.noreply.github.com>
2024-12-24 11:42:22 +0100

    Added SSH connection via Fabric

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:3.10
 
 WORKDIR /usr/src/app
 
-RUN pip install --no-cache-dir requests 
+RUN pip install --no-cache-dir requests fabric prometheus_client 
 
 COPY . .
 
--- slurm_exporter.py
+++ slurm_exporter.py
@@ -1,6 +1,61 @@
+import os 
 import sys
 import requests
+from fabric import Connection
+import time
+from prometheus_client import start_http_server, Gauge
+
+SSH_HOST = os.environ.get('SLURM_SSH_HOST')
+SSH_USER = os.environ.get('SLURM_SSH_USER')
+SSH_PASS = os.environ.get('SLURM_SSH_PASS')
+SSH_PORT = os.environ.get('SLURM_SSH_PORT')   
 
 
-print("Hello world!")
-print(requests.__version__)
+class RemoteClient:
+    def __init__(self, hostname: str, user: str, password: str, port: int):
+        """
+        Initialize the RemoteClient with connection details.
+
+        :param hostname: The hostname or IP address of the remote machine.
+        :param user: The username for the remote machine.
+        :param password: The password for the remote machine.
+        :param port: The SSH port to connect to.
+        """
+        self.hostname = hostname
+        self.user = user
+        self.password = password
+        self.port = port
+
+    def run_command(self, command: str) -> str:
+        """
+        Run a command on the remote machine.
+
+        :param command: The shell command to execute.
+        :return: The command's output.
+        """
+        try:
+            # Create a connection to the remote client
+            connection = Connection(
+                host=self.hostname,
+                user=self.user,
+                connect_kwargs={"password": self.password},
+                port=self.port
+            )
+
+            # Run the command and capture the result
+            result = connection.run(f"bash -lc '{command}'", hide=True)
+            
+            # Close the connection
+            connection.close()
+
+            return result.stdout.strip()
+        except Exception as e:
+            return f"An error occurred: {str(e)}"
+
+if __name__ == "__main__":
+    # Create a RemoteClient instance
+    client = RemoteClient(SSH_HOST, SSH_USER, SSH_PASS, SSH_PORT)
+
+    num_jobs = client.run_command("squeue -h | wc -l")
+
+    print(f"Number of jobs: {num_jobs}")
